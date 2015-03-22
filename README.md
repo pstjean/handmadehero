af6913a6941e1d873ed915e1439df865d4e79f03
Peter St.Jean <peter@stjean.co>
2015-03-22 12:56:13 -0500

    Creating a window in Windows

---
+++ README.md
@@ -0,0 +1,3 @@
+# Handmade Hero
+
+Following along with Casey Muratori's [Handmade Hero](https://handmadehero.org) project to build a full game from scratch in Windows using C++.
---
+++ misc/shell.bat
@@ -0,0 +1,3 @@
+@echo off
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x64
+set path=C:\Program Files (x86)\Git\bin;C:\Program Files (x86)\Git\cmd;C:\Program Files (x86)\Vim\vim74;w:\handmade\misc;%path%
